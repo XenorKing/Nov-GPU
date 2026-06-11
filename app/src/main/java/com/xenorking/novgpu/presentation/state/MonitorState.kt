9a4bcd6140973bd9a41e155599bbe814d85f0af1
XenorKing <da1ysss362@gmail.com>
2026-06-11 22:15:18 +0300

    feat: add app/src/main/java/com/xenorking/novgpu/presentation/state/MonitorState.kt

---
+++ app/src/main/java/com/xenorking/novgpu/presentation/state/MonitorState.kt
@@ -0,0 +1,21 @@
+package com.xenorking.novgpu.presentation.state
+
+import com.xenorking.novgpu.domain.model.MonitorConfig
+import com.xenorking.novgpu.domain.model.SystemStats
+
+data class MonitorState(
+    val stats: SystemStats = SystemStats(),
+    val config: MonitorConfig = MonitorConfig(),
+    val isLoading: Boolean = true,
+    val error: String? = null,
+    val selectedTab: Int = 0,
+    val isMonitoring: Boolean = false
+)
+
+sealed class MonitorIntent {
+    object StartMonitoring : MonitorIntent()
+    object StopMonitoring : MonitorIntent()
+    data class SelectTab(val index: Int) : MonitorIntent()
+    data class UpdateConfig(val config: MonitorConfig) : MonitorIntent()
+    data class SetGpuInfo(val renderer: String, val vendor: String, val version: String) : MonitorIntent()
+}
